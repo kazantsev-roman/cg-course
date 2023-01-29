3fe7697dee665cb21dc7dfeb2c4601ecc6a9e5ff
kazantsev-roman <dj.ka3anova@gmail.com>
2023-01-29 13:17:34 +0300

    сделал js-файл более читаемым, изменил id с svg на image

--- lab1/task2/script.js
+++ lab1/task2/script.js
@@ -1,44 +1,48 @@
-const image = document.getElementById('svg')
 const pageWidth = 5760
 const pageHeight = 2937
 
-image.onmousedown = function(event) {
-    let shiftX = event.clientX - image.getBoundingClientRect().left
-    let shiftY = event.clientY - image.getBoundingClientRect().top
+let shiftX = 0
+let shiftY = 0
 
-    function onMouseDown() {
-        image.style.position = 'absolute'
-        image.style.zIndex = '1000'
+const image = document.getElementById('image')
 
-        document.addEventListener('mousemove', onMouseMove)
-        document.addEventListener('mouseup', onMouseUp)
-    }
+function onMouseDown(event)
+{
+    shiftX = event.clientX - image.getBoundingClientRect().left
+    shiftY = event.clientY - image.getBoundingClientRect().top
 
-    onMouseDown()
+    image.style.position = 'absolute'
+    image.style.zIndex = '1000'
 
-    function onMouseMove(event) {
-        let positionX = event.pageX - shiftX
-        let positionY = event.pageY - shiftY
-
-        const minBorderX = 0
-        const maxBorderX = pageWidth - image.getBoundingClientRect().width
-
-        const minBorderY = 0
-        const maxBorderY = pageHeight - image.getBoundingClientRect().height
-
-        positionX = positionX < minBorderX ? minBorderX : positionX
-        positionX = positionX > maxBorderX ? maxBorderX : positionX
-
-        positionY = positionY < minBorderY ? minBorderY : positionY
-        positionY = positionY > maxBorderY ? maxBorderY : positionY
-
-        image.style.left = positionX + 'px'
-        image.style.top = positionY + 'px'
-    }
-
-    function onMouseUp() {
-        document.removeEventListener('mousemove', onMouseMove)
-        document.removeEventListener('mouseup', onMouseUp)
-    }
-
+    document.addEventListener('mousemove', onMouseMove)
+    document.addEventListener('mouseup', onMouseUp)
 }
+
+function onMouseMove(event)
+{
+    let positionX = event.pageX - shiftX
+    let positionY = event.pageY - shiftY
+
+    const minBorderX = 0
+    const maxBorderX = pageWidth - image.getBoundingClientRect().width
+
+    const minBorderY = 0
+    const maxBorderY = pageHeight - image.getBoundingClientRect().height
+
+    positionX = positionX < minBorderX ? minBorderX : positionX
+    positionX = positionX > maxBorderX ? maxBorderX : positionX
+
+    positionY = positionY < minBorderY ? minBorderY : positionY
+    positionY = positionY > maxBorderY ? maxBorderY : positionY
+
+    image.style.left = positionX + 'px'
+    image.style.top = positionY + 'px'
+}
+
+function onMouseUp()
+{
+    document.removeEventListener('mousemove', onMouseMove)
+    document.removeEventListener('mouseup', onMouseUp)
+}
+
+image.onmousedown = onMouseDown
