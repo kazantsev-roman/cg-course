0b66c01c27022157318560faf5b6a23ef4125202
kazantsev-roman <dj.ka3anova@gmail.com>
2023-02-02 15:15:01 +0300

    добавил модули файлов

--- lab1/task4/game/src/components/Game.tsx
+++ lab1/task4/game/src/components/Game.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import Gallows from "./Gallows/Gallows";
+import FieldOfDreams from "./FieldOfDreams/FieldOfDreams";
+import SettingsModal from "./SettingsModal/SettingsModal";
+import Settings from "./Settings/Settings";
+
 function Game()
 {
+	const [viewMode, setViewMode] = useState<'gallows' | 'field of dreams'>('gallows')
+	const [viewModal, setViewModal] = useState(false)
+
 	return (
-		<></>
+		<>
+			<Settings setViewModal={setViewModal} />
+			{
+				viewMode === 'gallows'
+					? <Gallows />
+					: <FieldOfDreams />
+			}
+			{
+				viewModal && <SettingsModal setViewMode={setViewMode} setViewModal={setViewModal} />
+			}
+		</>
 	)
 }
 
