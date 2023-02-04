813439c568e6bcb1458e8ed8dc2c7ad3003a77ae
kazantsev-roman <dj.ka3anova@gmail.com>
2023-02-04 18:54:20 +0300

    вынес повоторяющиеся данные в константу

--- lab1/task4/game/src/store/reducer/reducer.ts
+++ lab1/task4/game/src/store/reducer/reducer.ts
@@ -5,29 +5,28 @@ import { MARK_LETTER_AS_USED, NEW_GAME } from "../constants/constants";
 import Action from "../actions/actionType/ActionType";
 
 const reducer = (state: GameState = initialState , action: Action): GameState => {
+	const usedLetter = {
+		...action.payload as LetterData,
+		used: true
+	}
+
 	switch(action.type)
 	{
 	case MARK_LETTER_AS_USED:
 		return {
 			...state,
 			alphabet: state.alphabet.map((letter) => {
-				if ((action.payload as LetterData).letter === letter.letter)
+				if (usedLetter.letter === letter.letter)
 				{
-					return {
-						...letter,
-						used: true
-					}
+					return usedLetter
 				}
 				return letter
 			}),
 			usedLetters: [
 				...state.usedLetters,
-				{
-					...action.payload as LetterData,
-					used: true
-				}
+				usedLetter
 			],
-			numberOfAttempts: (action.payload as LetterData).correct ? state.numberOfAttempts : --state.numberOfAttempts
+			numberOfAttempts: usedLetter.correct ? state.numberOfAttempts : --state.numberOfAttempts
 		}
 	case NEW_GAME:
 		return getInitialState()
