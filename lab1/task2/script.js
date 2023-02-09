2135ab7f29478bf34668f5c82237503be15a8efa
kazantsev-roman <dj.ka3anova@gmail.com>
2023-02-09 15:23:22 +0300

    сделал dragAndDrop универсальным, избавившись от глобальных переменных

--- lab1/task2/script.js
+++ lab1/task2/script.js
@@ -1,48 +1,51 @@
 const pageWidth = 1920
 const pageHeight = 979
 
-let shiftX = 0
-let shiftY = 0
+function dragAndDrop(element)
+{
+    let shiftX = 0
+    let shiftY = 0
+
+    function onMouseDown(event)
+    {
+        shiftX = event.clientX - element.getBoundingClientRect().left
+        shiftY = event.clientY - element.getBoundingClientRect().top
+
+        document.addEventListener('mousemove', onMouseMove)
+        document.addEventListener('mouseup', onMouseUp)
+    }
+
+    function onMouseMove(event)
+    {
+        let positionX = event.pageX - shiftX
+        let positionY = event.pageY - shiftY
+
+        const minBorderX = 0
+        const maxBorderX = pageWidth - element.getBoundingClientRect().width
+
+        const minBorderY = 0
+        const maxBorderY = pageHeight - element.getBoundingClientRect().height
+
+        positionX = positionX < minBorderX ? minBorderX : positionX
+        positionX = positionX > maxBorderX ? maxBorderX : positionX
+
+        positionY = positionY < minBorderY ? minBorderY : positionY
+        positionY = positionY > maxBorderY ? maxBorderY : positionY
+
+        element.style.left = positionX + 'px'
+        element.style.top = positionY + 'px'
+    }
+
+    function onMouseUp()
+    {
+        document.removeEventListener('mousemove', onMouseMove)
+        document.removeEventListener('mouseup', onMouseUp)
+    }
+
+    element.style.position = 'absolute'
+    element.onmousedown = onMouseDown
+}
 
 const image = document.getElementById('image')
 
-function onMouseDown(event)
-{
-    shiftX = event.clientX - image.getBoundingClientRect().left
-    shiftY = event.clientY - image.getBoundingClientRect().top
-
-    image.style.position = 'absolute'
-    image.style.zIndex = '1000'
-
-    document.addEventListener('mousemove', onMouseMove)
-    document.addEventListener('mouseup', onMouseUp)
-}
-
-function onMouseMove(event)
-{
-    let positionX = event.pageX - shiftX
-    let positionY = event.pageY - shiftY
-
-    const minBorderX = 0
-    const maxBorderX = pageWidth - image.getBoundingClientRect().width
-
-    const minBorderY = 0
-    const maxBorderY = pageHeight - image.getBoundingClientRect().height
-
-    positionX = positionX < minBorderX ? minBorderX : positionX
-    positionX = positionX > maxBorderX ? maxBorderX : positionX
-
-    positionY = positionY < minBorderY ? minBorderY : positionY
-    positionY = positionY > maxBorderY ? maxBorderY : positionY
-
-    image.style.left = positionX + 'px'
-    image.style.top = positionY + 'px'
-}
-
-function onMouseUp()
-{
-    document.removeEventListener('mousemove', onMouseMove)
-    document.removeEventListener('mouseup', onMouseUp)
-}
-
-image.onmousedown = onMouseDown
+dragAndDrop(image)
