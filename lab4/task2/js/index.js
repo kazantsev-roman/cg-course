5fe7b22270b9259dd043ad0359708967be922452
kazantsev-roman <dj.ka3anova@gmail.com>
2023-04-20 09:50:14 +0300

    реаоизовал ленту мёбиуса

--- lab4/task2/js/index.js
+++ lab4/task2/js/index.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js'
-import { ParametricGeometries } from 'three/addons/geometries/ParametricGeometries.js'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
@@ -11,15 +10,30 @@ renderer.setClearColor(0x000000, 0)
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
-const geometry = new ParametricGeometry( ParametricGeometries.mobius3d, 999, 999 )
+const mobiusFunction = ( u, v, target ) => {
+    u *= 2 * Math.PI
+    v *= 2 * Math.PI
+
+    const a = 0.025
+    const b = 0.65
+
+    const k = a * Math.cos( v ) * Math.cos( u / 2 ) - b * Math.sin( v ) * Math.sin( u / 2 )
+
+    const x = ( 1 + k ) * Math.cos( u )
+    const y = ( 1 + k ) * Math.sin( u )
+    const z = a * Math.cos( v ) * Math.sin( u / 2 ) + b * Math.sin( v ) * Math.cos( u / 2 )
+
+    target.set( x, y, z )
+}
+
+const geometry = new ParametricGeometry( mobiusFunction, 100, 999 )
 const material = new THREE.ShaderMaterial({
     uniforms: {
         color1: { value: new THREE.Color("red") },
-        color2: { value: new THREE.Color("purple") }
+        color2: { value: new THREE.Color("green") }
     },
     vertexShader: vertexShaderText,
-    fragmentShader: fragmentShaderText,
-    wireframe: true
+    fragmentShader: fragmentShaderText
 })
 const mobius = new THREE.Mesh( geometry, material )
 scene.add( mobius )
@@ -27,12 +41,20 @@ scene.add( mobius )
 const controls = new OrbitControls( camera, renderer.domElement )
 controls.update()
 
-camera.position.z = 10
+camera.position.z = 5
 
 function animate() {
     requestAnimationFrame( animate )
     controls.update()
-
     renderer.render( scene, camera )
 }
 animate()
+
+function onWindowResize()
+{
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize( window.innerWidth, window.innerHeight )
+}
+
+window.addEventListener( 'resize', onWindowResize, false )
